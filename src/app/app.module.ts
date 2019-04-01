bb4e34f476e1ed1354967708b36ffc1d9ecc72d6
Meir Donath <kingdonath@gmail.com>
2019-03-31 20:25:26 -0400

    Added episode class. Fixed up models folder. Need to fix views.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SearchbarComponent } from './views/search/search.component';
+import { SeasonsComponent } from './views/seasons/seasons.component';
+import { ShowsComponent } from './views/shows/shows.component';
+import { EpisodesComponent } from './views/episodes/episodes.component';
+import { ServicesComponent } from './models/services/services.component';
+import {MatAutocompleteModule, MatIconModule, MatInputModule, MatOptionModule} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchbarComponent,
+    SeasonsComponent,
+    ShowsComponent,
+    EpisodesComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatOptionModule,
+    MatAutocompleteModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/episodes.ts
@@ -0,0 +1,21 @@
+export class Episodes {
+  id: number;
+  name: string;
+  number: number;
+  season: number;
+  dateAired: Date;
+  runtime: string;
+  summary: string;
+
+  constructor(args?) {
+    if (args) {
+      this.id = args.id;
+      this.name = args.name;
+      this.number = args.number;
+      this.season = args.season;
+      this.dateAired = args.dateAired;
+      this.runtime = args.runtime;
+      this.summary = args.summary;
+    }
+  }
+}
