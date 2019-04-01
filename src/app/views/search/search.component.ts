1d18ecb0838d3b9921004704c8e879477bdff1f1
Meir Donath <kingdonath@gmail.com>
2019-03-31 20:31:43 -0400

    Added shows, seasons and episodes in views.

---
+++ src/app/views/search/search.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from '@angular/core';
+import {Observable, Subject} from 'rxjs';
+import {TvMazeService} from '../../models/services.service';
+import {Show} from '../../models/shows.service';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
+})
+export class SearchComponent implements OnInit {
+  shows$: Observable<Show[]>;
+  private searchTerms = new Subject<string>();
+
+  constructor(private tvMazeService: TvMazeService) {
+  }
+
+  ngOnInit() {
+  }
+
+  // Push a search term into the observable stream.
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+}
